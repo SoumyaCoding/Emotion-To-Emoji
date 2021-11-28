122795949d9d2ebfe0d2598a5eba976d9a01f7b5
SoumyaCoding <86145656+SoumyaCoding@users.noreply.github.com>
2021-11-28 19:18:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    img_format: "png",
+    png_quality: 90,
+})
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/5mxzZHkpx/model.json',modelLoaded);
+    })
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+
+function speak()
+{
+var synth = window.SpeechSynthesis;
+speak_data_1 = "The first prediction is + prediction_1";
+speak_data_2 = "And the first prediction is + prediction_2";
+var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+synth.speak(utterThis);
+}
